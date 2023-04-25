cbdb70fbe29ce3a4430b917c5e6f40d278516a30
cmatthe2 <cmatthe2@gustavus.edu>
2023-04-25 13:05:38 -0500

    Added OMS (OpenStreetMap)

--- app/src/main/java/com/example/fishbook/map/Map.java
+++ app/src/main/java/com/example/fishbook/map/Map.java
@@ -1,6 +1,49 @@
 package com.example.fishbook.map;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import com.example.fishbook.R;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
 public class Map extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        Context ctx = getActivity().getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        MapView map = view.findViewById(R.id.map);
+
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+
+        //Set Minnesota Coordinates
+        mapController.setZoom(8.1);
+        GeoPoint startPoint = new GeoPoint(46.7296, -94.6859);
+        mapController.setCenter(startPoint);
+
+        return view;
+    }
+    public void onResume(){
+        super.onResume();
+        Configuration.getInstance().load(getActivity(), PreferenceManager.getDefaultSharedPreferences(getActivity()));
+    }
 }
