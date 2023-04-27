d112633ad9161db6b2ddd025d65b99e7b764966e
charliehora <charliehora@gmail.com>
2023-04-26 21:53:07 -0500

    remember login

--- app/src/main/java/com/example/fishbook/auth/LoginPage.kt
+++ app/src/main/java/com/example/fishbook/auth/LoginPage.kt
@@ -1,12 +1,12 @@
 package com.example.fishbook.auth
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.example.fishbook.MainActivity
 import com.example.fishbook.R
 import com.google.firebase.auth.FirebaseAuth
@@ -17,15 +17,16 @@ class LoginPage : AppCompatActivity() {
     lateinit var editEmail: EditText
     private lateinit var editPass: EditText
     lateinit var btnLogin: Button
-
-
+    lateinit var checkBox: CheckBox
+    lateinit var preferences: SharedPreferences
+    lateinit var editor: SharedPreferences.Editor
     lateinit var auth: FirebaseAuth
-
+    lateinit var check: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-
+        checkBox = findViewById(R.id.checkBoxRememberMe)
         textView = findViewById(R.id.registerNow)
         btnLogin = findViewById(R.id.btn_login)
         editEmail = findViewById(R.id.email)
@@ -33,6 +34,15 @@ class LoginPage : AppCompatActivity() {
 
 
         auth = FirebaseAuth.getInstance()
+
+        preferences = getSharedPreferences("checkbox", MODE_PRIVATE)
+        check = preferences.getString("remember", "").toString()
+        if(check.equals("true")){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }else if(check.equals("false")){
+            Toast.makeText(this, "Please sign in.", Toast.LENGTH_SHORT).show()
+        }
 
         btnLogin.setOnClickListener {
             login()
@@ -44,6 +54,28 @@ class LoginPage : AppCompatActivity() {
 
             finish()
         }
+
+        checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (checkBox.isChecked()) {
+                preferences = getSharedPreferences("checkbox", MODE_PRIVATE)
+                editor = preferences.edit()
+                editor.putString("remember", "true")
+                editor.apply()
+                Toast.makeText(this, "Checked", Toast.LENGTH_SHORT).show()
+
+            } else if (!checkBox.isChecked()) {
+                preferences = getSharedPreferences("checkbox", MODE_PRIVATE)
+                editor = preferences.edit()
+                editor.putString("remember", "false")
+                editor.apply()
+                Toast.makeText(this, "Unchecked", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+
+
+
     }
 
     private fun login() {
@@ -52,12 +84,14 @@ class LoginPage : AppCompatActivity() {
 
         auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
             if (it.isSuccessful) {
-                Toast.makeText(this, "Successfully LoggedIn", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             } else
                 Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
         }
     }
+
+
 
 }
--- app/src/main/java/com/example/fishbook/fishdex/SpeciesRepository.kt
+++ app/src/main/java/com/example/fishbook/fishdex/SpeciesRepository.kt
@@ -27,13 +27,13 @@ class SpeciesRepository private constructor(context: Context) {
         if (database.SpeciesDao().getSpeciesCount() == 0) {
             val speciesList = listOf(
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Bluegill",
                     fish_family = "Sunfish",
                     image = R.drawable.fish_sunbluegill
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Green Sunfish",
                     fish_family = "Sunfish",
                     image = R.drawable.fish_sungreen
@@ -45,31 +45,31 @@ class SpeciesRepository private constructor(context: Context) {
                     image = R.drawable.fish_sunpumpkin
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Rock Bass",
                     fish_family = "Sunfish",
                     image = R.drawable.fish_rockbass
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Black Crappie",
                     fish_family = "Crappie",
                     image = R.drawable.fish_crappiebl
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "White Crappie",
                     fish_family = "Crappie",
                     image = R.drawable.fish_crappiewh
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Largemouth Bass",
                     fish_family = "Bass",
                     image = R.drawable.fish_lmbass
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Smallmouth Bass",
                     fish_family = "Bass",
                     image = R.drawable.fish_smbass
@@ -77,13 +77,13 @@ class SpeciesRepository private constructor(context: Context) {
 
 
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Bullhead",
                     fish_family = "Catfish",
                     image = R.drawable.fish_bullheadcat
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Channel Catfish",
                     fish_family = "Catfish",
                     image = R.drawable.fish_channelcat
@@ -95,19 +95,19 @@ class SpeciesRepository private constructor(context: Context) {
                     image = R.drawable.fish_flatheadcat
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Yellow Perch",
                     fish_family = "Perch",
                     image = R.drawable.fish_yellowperch
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Walleye",
                     fish_family = "Perch",
                     image = R.drawable.fish_walleye
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Northern Pike",
                     fish_family = "Pike",
                     image = R.drawable.fish_pike
@@ -119,13 +119,13 @@ class SpeciesRepository private constructor(context: Context) {
                     image = R.drawable.fish_musky
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Brook Trout",
                     fish_family = "Trout",
                     image = R.drawable.fish_troutbrook
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Brown Trout",
                     fish_family = "Trout",
                     image = R.drawable.fish_troutbrown
@@ -137,19 +137,19 @@ class SpeciesRepository private constructor(context: Context) {
                     image = R.drawable.fish_troutrainbow
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Longnose Gar",
                     fish_family = "Gar",
                     image = R.drawable.fish_garln
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Shortnose Gar",
                     fish_family = "Gar",
                     image = R.drawable.fish_garsn
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Eelpout",
                     fish_family = "Oddball",
                     image = R.drawable.fish_eelpout
@@ -167,19 +167,19 @@ class SpeciesRepository private constructor(context: Context) {
                     image = R.drawable.fish_buffalo
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Common Carp",
                     fish_family = "Oddball",
                     image = R.drawable.fish_carp
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "Northern Hogsucker",
                     fish_family = "Oddball",
                     image = R.drawable.fish_hogsucker
                 ),
                 Species(
-                    caught_flag = true,
+                    caught_flag = false,
                     species_name = "White Bass",
                     fish_family = "Oddball",
                     image = R.drawable.fish_whitebass
