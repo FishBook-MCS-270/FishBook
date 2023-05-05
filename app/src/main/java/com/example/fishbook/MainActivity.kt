995fba615d62b45453122d6b04513cdab2e998c6
cmatthe2 <cmatthe2@gustavus.edu>
2023-05-04 21:08:03 -0500

    User and current location

--- app/src/main/java/com/example/fishbook/MainActivity.kt
+++ app/src/main/java/com/example/fishbook/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.fishbook
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.fishbook.fishdex.DataRepository
 import com.example.fishbook.gallery.ImageRepository
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomNavigationView: BottomNavigationView
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         DataRepository.initialize(this)
@@ -24,5 +33,24 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         bottomNavigationView.setupWithNavController(navController)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fetchLocation()
+    }
+
+    private fun fetchLocation() {
+        val task = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            Log.d(TAG, "Request permission")
+        }
+        task.addOnSuccessListener {
+            if(it != null){
+                Log.d(TAG, "Current Location: latitude: " + it.latitude + ", longitude: " + it.longitude)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/fishbook/map/Map.java
+++ app/src/main/java/com/example/fishbook/map/Map.java
@@ -11,13 +11,18 @@ import androidx.fragment.app.Fragment;
 
 import com.example.fishbook.R;
 
-import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class Map extends Fragment {
+
+    private MapView map = null;
+
+    private MapController mapController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,24 +31,34 @@ public class Map extends Fragment {
         Context ctx = getActivity().getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
 
-        MapView map = view.findViewById(R.id.map);
+        map = view.findViewById(R.id.map);
 
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
-        IMapController mapController = map.getController();
+        mapController = (MapController) map.getController();
 
         //Set Minnesota Coordinates
         mapController.setZoom(8.1);
         GeoPoint startPoint = new GeoPoint(46.7296, -94.6859);
         mapController.setCenter(startPoint);
 
+        MyLocationNewOverlay myLocationNewOverlay = new MyLocationNewOverlay(map);
+        myLocationNewOverlay.enableFollowLocation();
+        myLocationNewOverlay.enableMyLocation();
+        map.getOverlays().add(myLocationNewOverlay);
+
         return view;
     }
     public void onResume(){
         super.onResume();
-        Configuration.getInstance().load(getActivity(), PreferenceManager.getDefaultSharedPreferences(getActivity()));
+        map.onResume();
+    }
+
+    public void onPause(){
+        super.onPause();
+        map.onPause();
     }
 }
