e45f29872209c97d339a78952eb232ba44c2b867
Cameron Boelter <camjboelter@gmail.com>
2023-05-07 20:08:33 -0500

    update onCreate()

--- app/src/main/java/com/example/fishbook/storage/Application.kt
+++ app/src/main/java/com/example/fishbook/storage/Application.kt
@@ -2,12 +2,38 @@ package com.example.fishbook.storage
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import com.squareup.picasso.LruCache
+import com.squareup.picasso.OkHttp3Downloader
+import com.squareup.picasso.Picasso
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import java.io.File
 
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
         DataRepository.initialize(this)
         FirebaseApp.initializeApp(this)
+// Memory cache configuration
+        val cacheSize = 50 * 1024 * 1024 // 50 MB
+        val memoryCache = LruCache(cacheSize)
 
+        // Disk cache configuration
+        val cacheDir = File(cacheDir, "picasso-cache")
+        if (!cacheDir.exists()) {
+            cacheDir.mkdirs()
+        }
+        val httpClient = OkHttpClient.Builder()
+            .cache(Cache(cacheDir, cacheSize.toLong()))
+            .build()
+
+        // Create custom Picasso instance with configured memory and disk cache
+        val customPicasso = Picasso.Builder(this)
+            .memoryCache(memoryCache)
+            .downloader(OkHttp3Downloader(httpClient))
+            .build()
+
+        // Set the custom Picasso instance as the singleton instance
+        Picasso.setSingletonInstance(customPicasso)
     }
 }
