17c9d94fb8ca8cb297a5a71bb270ca81b505ce2a
cmatthe2 <cmatthe2@gustavus.edu>
2023-05-09 00:45:59 -0500

    Added features to the map and set limit zoom in/out.

--- app/src/main/java/com/example/fishbook/map/Map.kt
+++ app/src/main/java/com/example/fishbook/map/Map.kt
@@ -13,6 +13,11 @@ import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.ScaleBarOverlay
+import org.osmdroid.views.overlay.compass.CompassOverlay
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class Map : Fragment() {
@@ -27,6 +32,9 @@ class Map : Fragment() {
         val ctx = requireActivity().applicationContext
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
         val map = view.findViewById<MapView>(R.id.map)
+
+        map.setUseDataConnection(true)
+
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.setBuiltInZoomControls(true)
         map.setMultiTouchControls(true)
@@ -36,12 +44,39 @@ class Map : Fragment() {
         mapController.setZoom(8.1)
         val startPoint = GeoPoint(46.7296, -94.6859)
         mapController.setCenter(startPoint)
-        val myLocationNewOverlay = MyLocationNewOverlay(map)
-        myLocationNewOverlay.enableFollowLocation()
+
+        //Set Zoom Limit
+        map.minZoomLevel = 3.0
+        map.maxZoomLevel = 19.0
+
+        //Set Current Location
+        val mGpsMyLocationProvider = GpsMyLocationProvider(activity)
+        mGpsMyLocationProvider.locationUpdateMinDistance
+        mGpsMyLocationProvider.locationUpdateMinTime
+        val myLocationNewOverlay = MyLocationNewOverlay(mGpsMyLocationProvider, map)
         myLocationNewOverlay.enableMyLocation()
+        myLocationNewOverlay.enableFollowLocation()
+        myLocationNewOverlay.isDrawAccuracyEnabled
         map.overlays.add(myLocationNewOverlay)
+
+        //Set Scale Bar
+        val mScaleBarOverlay = ScaleBarOverlay(map)
+        mScaleBarOverlay.setScaleBarOffset(resources.displayMetrics.widthPixels / 2, 10)
+        mScaleBarOverlay.setAlignBottom(true)
+        map.overlays.add(mScaleBarOverlay)
+
+        //Support for Map Rotation
+        val mRotationGestureOverlay = RotationGestureOverlay(map)
+        mRotationGestureOverlay.isEnabled
+        map.overlays.add(myLocationNewOverlay)
+
+        //Set Compass
+        val mCompassOverlay = CompassOverlay(activity, InternalCompassOrientationProvider(activity), map)
+        map.overlays.add(mCompassOverlay)
+
         return view
     }
+
 
     override fun onResume() {
         super.onResume()
